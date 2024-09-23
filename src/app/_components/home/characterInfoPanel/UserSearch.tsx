888a190ab33b44f46bc40e0eaf1b0fe347fddf91
winter100 <100taeng@gmail.com>
2024-09-23 21:41:24 +0900

    refactor: 재사용을 위한 서치컴포넌트 리팩토링

--- src/app/_components/home/characterInfoPanel/UserSearch.tsx
+++ src/app/_components/home/characterInfoPanel/UserSearch.tsx
@@ -1,59 +1,34 @@
 "use client";
-
-import { FormEvent, useRef, useState } from "react";
-import { toast } from "react-toastify";
+import { FormEvent } from "react";
+import { Dispatch, RefObject, SetStateAction } from "react";
 import { GoSearch } from "react-icons/go";
 
 import Row from "../../layout/Row";
 import Input from "../../common/Input";
 import Loading from "../../common/Loading";
-import { useCharacterStore } from "@/app/_store/characterStore";
-import { useCharacter } from "@/app/_hooks/useCharacter/useCharacter";
 
-const UserSearch = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const { handleCharacterInfo, loading } = useCharacter();
-  const characters = useCharacterStore((state) => state.characters);
+interface UserSearchProps {
+  inputRef: RefObject<HTMLInputElement>;
+  isFocused: boolean;
+  loading: boolean;
+  onSubmitHandler: (e: FormEvent) => Promise<void>;
+  setIsFocused: Dispatch<SetStateAction<boolean>>;
+}
 
-  const [isFocused, setIsFocused] = useState(false);
-
-  const onClickHandler = async (e: FormEvent) => {
-    e.preventDefault();
-    const characterLength = characters.length;
-    if (!characters || characterLength >= 8) {
-      toast.error("캐릭터는 최대 8명까지 등록 가능합니다.");
-      return;
-    }
-    if (inputRef.current) {
-      const inputValue = inputRef.current.value;
-      const resultArray = inputValue
-        .trim()
-        .replace(/\s+/g, " ")
-        .split(" ")
-        .filter(Boolean)
-        .slice(0, 8 - characterLength);
-      if (resultArray.length >= 1) {
-        for (const item of resultArray) {
-          await handleCharacterInfo(item);
-        }
-      } else {
-        inputRef.current.focus();
-        toast.error("캐릭터 이름을 입력해주세요.", {
-          toastId: "characterName",
-        });
-      }
-      inputRef.current.focus();
-      inputRef.current.value = "";
-    }
-  };
-
+const UserSearch = ({
+  inputRef,
+  isFocused,
+  loading,
+  onSubmitHandler,
+  setIsFocused,
+}: UserSearchProps) => {
   return (
     <Row
       className={`${isFocused ? "border-blue-300" : ""} ${loading ? "bg-gray-200 opacity-60" : ""} outline-blue-300" h-8 w-60 gap-1 rounded-lg border-2 text-sm text-black shadow-sm`}
     >
       <form
         id="search"
-        onSubmit={onClickHandler}
+        onSubmit={onSubmitHandler}
         className="flex h-full w-full pl-2"
       >
         <Input
