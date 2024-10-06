3bcbcdf6f82b03f166d88985615a85ee7f40f9b9
winter100 <100taeng@gmail.com>
2024-10-06 12:36:35 +0900

    fix: 상한조회 높이 수정

--- src/app/(route)/(home)/page.tsx
+++ src/app/(route)/(home)/page.tsx
@@ -6,12 +6,12 @@ import Section from "@/app/_components/layout/Section";
 export default function Home() {
   return (
     <>
-      <Section className="flex h-[450px] w-full items-center justify-center">
+      <Section className="flex h-full max-h-[450px] w-full flex-1 items-center justify-center">
         <Row className="h-full w-full">
           <CharacterInfoPanel />
         </Row>
       </Section>
-      <Section className="flex-1">
+      <Section className="h-full flex-1">
         <Row className="h-full">
           <CharacterDetailInfo />
         </Row>
