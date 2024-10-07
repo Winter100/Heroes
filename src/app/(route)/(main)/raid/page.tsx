300f3919dd54308bd1a27bc39020519c5f71d72b
winter100 <100taeng@gmail.com>
2024-10-07 16:53:56 +0900

    style: 순회페이지 flex-1로 변경

--- src/app/(route)/(main)/raid/page.tsx
+++ src/app/(route)/(main)/raid/page.tsx
@@ -4,9 +4,12 @@ import CharacterDetailInfo from "./_components/CharacterDetailInfo";
 export default function Home() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
-      <CharacterInfoPanel />
-
-      <CharacterDetailInfo />
+      <div className="w-full flex-1">
+        <CharacterInfoPanel />
+      </div>
+      <div className="w-full flex-1">
+        <CharacterDetailInfo />
+      </div>
     </div>
   );
 }
--- src/app/_components/home/characterInfoPanel/AbilityRankList.tsx
+++ src/app/_components/home/characterInfoPanel/AbilityRankList.tsx
@@ -7,7 +7,7 @@ const NoSSRAbilityRankTbody = dynamic(() => import("./AbilityRankTbody"), {
 
 const AbilityRankList = () => {
   return (
-    <table className="flex h-full max-h-[410px] w-full table-auto flex-col gap-1">
+    <table className="flex h-full w-full table-auto flex-col gap-1 sm:max-h-[410px]">
       <caption className="hidden">캐릭터 리스트</caption>
       <AbilityRankThead />
       <NoSSRAbilityRankTbody />
