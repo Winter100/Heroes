90cdca09849c5e7ca2189786657d740b88f3aa61
winter100 <100taeng@gmail.com>
2025-03-14 15:38:17 +0900

    feat: shadcn/ui 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -23,15 +24,22 @@ const config: Config = {
       },
       keyframes: {
         boundUpDown: {
-          "0%": { transform: "translateY(0)" },
-          "50%": { transform: "translateY(-3px)" },
-          "100%": { transform: "translateY(0)" },
+          "0%": {
+            transform: "translateY(0)",
+          },
+          "50%": {
+            transform: "translateY(-3px)",
+          },
+          "100%": {
+            transform: "translateY(0)",
+          },
         },
       },
       colors: {
         background: "rgba(var(--background))",
         backgroundOne: "rgba(var(--backgroundOne))",
         backgroundTwo: "rgba(var(--backgroundTwo))",
+        ["backgroundTap-1"]: "rgba(var(--backgroundTap-1))",
         btnHoverBackground: "rgba(var(--btnHoverBackground))",
         fontColor: "rgba(var(--fontColor))",
         gnbBackground: "rgba(var(--gnbBackground))",
@@ -40,12 +48,56 @@ const config: Config = {
         toggleBackground: "rgba(var(--toggleBackground))",
         deleteFontColor: "rgba(var(--deleteFontColor))",
         selectedOrHoverColor: "rgba(var(--selectedOrHoverColor))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
       },
       translate: {
         "-1/2": "-50%",
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
