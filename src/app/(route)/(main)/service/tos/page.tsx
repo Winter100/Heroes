9d5ee2336805e0bd48ec209e24a0d5f889d246f4
winter100 <100taeng@gmail.com>
2025-04-09 15:58:26 +0900

    feat: 이용약관 및 개인정보처리방침 추가

---
+++ src/app/(route)/(main)/service/tos/page.tsx
@@ -0,0 +1,9 @@
+import TermsGuide from "@/app/_components/service/TermsGuide";
+import { TermsData } from "@/app/_constant/service/terms";
+import React from "react";
+
+const Page = () => {
+  return <TermsGuide title="이용약관" data={TermsData} />;
+};
+
+export default Page;
