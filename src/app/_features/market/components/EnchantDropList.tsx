e719dfbf0f855666e79407d868477b4100073c91
winter100 <100taeng@gmail.com>
2025-07-15 13:26:28 +0900

    refactor: 인챈트 정보 리팩토링

--- src/app/_features/market/components/EnchantDropList.tsx
+++ src/app/_features/market/components/EnchantDropList.tsx
@@ -1,74 +1,48 @@
-import { memo } from 'react';
-import Image from 'next/image';
+import { cn } from '@/lib/utils';
 import { getImageByName } from '@/app/_utils/getImageByName';
-import { EnchantStoreType } from '@/app/_store/selectEnchantStore';
-import clsx from 'clsx';
-import Column from '@/app/_components/layout/Column';
-import { useEnchantFilterStore } from '../store/enchantFilterStore';
+import ImageIcon from '@/app/_components/common/Image-Icon';
 
+interface EnchantDropListProps {
+  dropList: string[];
+  setDropRaidOrItemName: (enchantName: string) => void;
+  dropRaidOrItemName: string;
+}
 const EnchantDropList = ({
-  enchantData,
-}: {
-  enchantData: EnchantStoreType;
-}) => {
-  const { dropRaidOrItemName, setDropRaidOrItemName } = useEnchantFilterStore(
-    (state) => ({
-      dropRaidOrItemName: state.dropRaidOrItemName,
-      setDropRaidOrItemName: state.setDropRaidOrItemName,
-    })
-  );
-
-  if (enchantData.drop_item_list?.length === 0 || !enchantData.drop_item_list) {
+  dropList,
+  setDropRaidOrItemName,
+  dropRaidOrItemName,
+}: EnchantDropListProps) => {
+  if (dropList?.length === 0 || !dropList) {
     return (
-      <div className="flex flex-1 items-center justify-center rounded-md border border-borderColor/50 p-2 font-sans text-sm font-light text-white">
+      <div className="flex flex-1 items-center justify-center rounded-md border border-muted p-2 font-sans text-sm font-light text-white">
         추가 예정
       </div>
     );
   }
 
   return (
-    <Column className="flex-1 rounded-md border border-borderColor/50 p-1">
-      <ul className="grid w-full grid-cols-3 gap-1 text-sm text-white">
-        {enchantData.drop_item_list?.sort().map((name) => (
-          <li
-            onClick={() => setDropRaidOrItemName(name)}
-            className={clsx(
-              'flex cursor-pointer flex-col items-center justify-center rounded-md hover:animate-boundUpDown',
-              dropRaidOrItemName === name
-                ? 'rounded-md border border-blue-300 text-blue-300'
-                : 'hover:outline hover:outline-1 hover:outline-rose-300'
-            )}
-            key={name}
-          >
-            <div className="flex h-20 items-center justify-center">
-              <Image
-                width={60}
-                height={60}
-                className="object-scale-down"
-                title={name}
-                src={getImageByName(name)}
-                alt={name}
-              />
-            </div>
-
-            <span className="flex-shrink-0 px-1 text-center text-xs">
-              {name}
-            </span>
-          </li>
-        ))}
-      </ul>
-    </Column>
+    <ul className="grid h-full max-h-56 w-full flex-1 grid-cols-3 gap-1 overflow-y-auto rounded-md border border-muted p-1 text-sm text-white">
+      {dropList?.sort().map((name: string) => (
+        <li
+          onClick={() => setDropRaidOrItemName(name)}
+          className={cn(
+            'flex cursor-pointer flex-col items-center justify-center rounded-md hover:animate-boundUpDown',
+            dropRaidOrItemName === name
+              ? 'rounded-md border border-blue-300 text-blue-300'
+              : 'hover:outline hover:outline-1 hover:outline-rose-300'
+          )}
+          key={name}
+        >
+          <ImageIcon
+            className="h-10 w-10"
+            src={getImageByName(name)}
+            alt={name}
+          />
+          <span className="flex-shrink-0 px-1 text-center text-xs">{name}</span>
+        </li>
+      ))}
+    </ul>
   );
 };
 
-const arePropsEqual = (
-  prevProps: { enchantData: EnchantStoreType },
-  nextProps: { enchantData: EnchantStoreType }
-) => {
-  return (
-    JSON.stringify(prevProps.enchantData.drop_item_list) ===
-    JSON.stringify(nextProps.enchantData.drop_item_list)
-  );
-};
-
-export default memo(EnchantDropList, arePropsEqual);
+export default EnchantDropList;
