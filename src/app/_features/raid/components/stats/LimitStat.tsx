fb4da4c986c771011f2a257a75563aa6287b015f
winter100 <100taeng@gmail.com>
2025-10-23 10:54:28 +0900

    chore: 테이블 조회 점검 표시

--- src/app/_features/raid/components/stats/LimitStat.tsx
+++ src/app/_features/raid/components/stats/LimitStat.tsx
@@ -1,5 +1,5 @@
-// import { useRankStore } from '@/app/_store/rankStore';
-// import { useCharacterStore } from '../../store/characterStore';
+import { useRankStore } from '@/app/_store/rankStore';
+import { useCharacterStore } from '../../store/characterStore';
 import { limitCalculator } from '@/app/_utils/raid/limitCalculator';
 import { LimitStatProps } from '../../types';
 
@@ -7,21 +7,21 @@ const LimitStat = (props: LimitStatProps) => {
   const selectedBoss = props.selectedBoss;
   const statName = props.stat_name;
   const statValue = props.stat_value;
-  // const characterName = props.characterName;
+  const characterName = props.characterName;
 
-  // const rankTitleList = useRankStore((state) => state?.rankTitleList);
-  // const isLimit = rankTitleList.find((t) => t.stat_name === '파괴력')?.isView;
+  const rankTitleList = useRankStore((state) => state?.rankTitleList);
+  const isLimit = rankTitleList.find((t) => t.stat_name === '파괴력')?.isView;
 
-  // const limitValue = useCharacterStore((state) => {
-  //   if (isLimit) {
-  //     return (
-  //       state?.characters
-  //         .find((c) => c?.name === characterName)
-  //         ?.stat.find((i) => i.stat_name === '파괴력')?.stat_value ?? '0'
-  //     );
-  //   }
-  //   return '0';
-  // });
+  const limitValue = useCharacterStore((state) => {
+    if (isLimit) {
+      return (
+        state?.characters
+          .find((c) => c?.name === characterName)
+          ?.stat.find((i) => i.stat_name === '파괴력')?.stat_value ?? '0'
+      );
+    }
+    return '0';
+  });
 
   const stat = limitCalculator(
     selectedBoss,
@@ -30,6 +30,16 @@ const LimitStat = (props: LimitStatProps) => {
     statValue?.toString()
     // limitValue?.toString()
   );
+
+  if (statName === '파괴력') {
+    return (
+      <span
+        className={`block text-center text-[10px] ${Number(stat) >= 0 ? 'text-green-300' : 'text-red-300'} `}
+      >
+        {limitValue}
+      </span>
+    );
+  }
 
   return (
     <>
