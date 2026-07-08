13344a5e5b3886d9fa9bf8603e941cf2914734de
winter100 <100taeng@gmail.com>
2026-07-08 18:38:56 +0900

    refactor: 레시피 리팩토링

--- src/app/_features/iteminfo/components/crafting/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ItemCraftingMaterialList } from './ItemCraftingMaterialList';
-export { default as ItemCraftingMethod } from './ItemCraftingMethod';
-export { default as CraftingCount } from './CraftingCount';
-export { default as CraftingQuantity } from './CraftingQuantity';
--- src/app/_features/iteminfo/types.ts
+++
@@ -1,9 +0,0 @@
-import {
-  ItemListType,
-  MaterialsType,
-} from '@/app/_constant/items/item_crafting_materials_list';
-
-export interface ItemCraftingMaterialListProps {
-  craftingList: MaterialsType[];
-  itemCraftingMap: Map<string, ItemListType>;
-}
